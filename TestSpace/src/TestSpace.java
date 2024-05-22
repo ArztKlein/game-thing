acf7094a5371f2cfe0bbabb8184d023ded95f61d
ArztKlein <52363453+ArztKlein@users.noreply.github.com>
2024-05-22 15:28:39 +1200

    fix

--- TestSpace/src/TestSpace.java
+++ TestSpace/src/TestSpace.java
@@ -1,22 +1,23 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.Timer;
 
 public class TestSpace extends GameEngine {
-   // private final ParticleEmitter emitter = new AlienParticleEmitter();
+    private final ParticleEmitter emitter = new FireParticleEmitter();
+    private final MainMenu mainMenu = new MainMenu(this);
     public static final int WIDTH  = 600;
     public static final int HEIGHT = 840;
-    private BulletManager bulletManager;
-    private Timer shootingTimer;
-    private boolean shooting;
-    private final int RPM = 300;
-    private final int BULLET_INTERVAL = 200;
+    public static final BulletManager bulletManager = new BulletManager();
     private Player player;
     private AlienManager alienManager;
     private double timeSinceLastAlienSpawn = 0; // Handle time accumulation for alien spawning
     private final double ALIEN_SPAWN_INTERVAL = 0.5; // Adjust this value to control alien spawning rate
 
+    public enum State {
+        MAIN_MENU,
+        PLAYING
+    }
+
+    private State state = State.MAIN_MENU;
 
     Image background = loadImage("TestSpace/resources/LargeSpace.png");
 
@@ -26,40 +27,49 @@ public class TestSpace extends GameEngine {
 
     public void init() {
         setWindowSize(WIDTH, HEIGHT);
-        //emitter.move(300, 50);
-        this.bulletManager = new BulletManager(this.loadImage("TestSpace/resources/bullet.png"), HEIGHT);
-        this.player = new Player(WIDTH / 2, HEIGHT - 100, loadImage("TestSpace/resources/Spaceman.png"));
-        this.shooting = false;
-        this.alienManager = new AlienManager(this.loadImage("TestSpace/resources/Alien.png"), player); // Initialize the AlienManager
     }
+
+    public void startGame() {
+        this.player = new Player(WIDTH / 2, HEIGHT - 100, GameEngine.loadImage("TestSpace/resources/Spaceman.png"));
+        emitter.move((float)player.getX()+10, HEIGHT - 105);
+        state = State.PLAYING;
+        alienManager = new AlienManager(loadImage("TestSpace/resources/Alien.png"), player); // Initialise the AlienManager
+    }
+
     @Override
     public void update(double dt) {
-       // emitter.update((float) dt);
-        player.update(dt);
-        // Update alien manager
-        alienManager.update(dt); // Update the aliens
-        // Handle alien spawning
-        timeSinceLastAlienSpawn += dt; // Accumulate time for alien spawning
-        if (timeSinceLastAlienSpawn >= ALIEN_SPAWN_INTERVAL) { // Check if it's time to spawn a new alien
-            alienManager.spawnAlien(); // Spawn a new alien
-            timeSinceLastAlienSpawn -= ALIEN_SPAWN_INTERVAL; // Reset the time accumulator
+        switch (state) {
+            case MAIN_MENU -> mainMenu.update();
+            case PLAYING -> {
+                player.update(dt);
+                emitter.update((float) dt);
+                bulletManager.updateBullets(dt);
+                alienManager.update(dt); // Update the aliens
+                // Handle alien spawning
+                timeSinceLastAlienSpawn += dt; // Accumulate time for alien spawning
+                if (timeSinceLastAlienSpawn >= ALIEN_SPAWN_INTERVAL) { // Check if it's time to spawn a new alien
+                    alienManager.spawnAlien(); // Spawn a new alien
+                    timeSinceLastAlienSpawn -= ALIEN_SPAWN_INTERVAL; // Reset the time accumulator
+                }
+            }
         }
     }
 
     @Override
     public void paintComponent() {
-      //  changeBackgroundColor(Color.BLACK);
+        changeBackgroundColour(black);
         clearBackground(WIDTH, HEIGHT);
 
-        drawBackground();
-
-        player.draw(this);
-
-       // drawEmitter();
-
-        bulletManager.drawBullets(this.mGraphics);
-        alienManager.draw(this.mGraphics);
-
+        switch (state) {
+            case MAIN_MENU -> mainMenu.draw();
+            case PLAYING -> {
+                drawBackground();
+                player.draw(this);
+//                drawEmitter();
+                bulletManager.drawBullets(mGraphics);
+                alienManager.draw(this.mGraphics);
+            }
+        }
     }
 
     public void drawBackground() {
@@ -69,24 +79,51 @@ public class TestSpace extends GameEngine {
         restoreLastTransform();
     }
 
-    public void drawEmitter() {
-    //    emitter.draw(this);
-    }
-
+//    public void drawEmitter() {
+//        emitter.move((float)player.getX()+10, HEIGHT - 105);
+//        emitter.draw(this);
+//    }
+    @Override
     public void keyPressed(KeyEvent event) {
-        switch (event.getKeyCode()) {
-            case (KeyEvent.VK_LEFT) -> player.moveLeft();
-            case (KeyEvent.VK_RIGHT) -> player.moveRight();
+        switch (state) {
+            case MAIN_MENU -> mainMenu.keyPressed(event);
+            case PLAYING -> {
+                switch (event.getKeyCode()) {
+                    case (KeyEvent.VK_LEFT) -> player.moveLeft();
+                    case (KeyEvent.VK_RIGHT) -> player.moveRight();
+                    case (KeyEvent.VK_SPACE) -> {
+                        if (!player.isShooting()) {
+                            player.startShooting();
+                        }
+                        emitter.emitterFrequency = 75;
+                    }
+                }
+            }
         }
     }
-
+    @Override
     public void keyReleased(KeyEvent event) {
-        if (event.getKeyCode() == KeyEvent.VK_LEFT && this.player.getDirection() == -1) {
-            this.player.stop();
+        if (state == State.PLAYING) {
+            switch (event.getKeyCode()) {
+                case (KeyEvent.VK_LEFT) -> {if(this.player.getDirection() == -1) {this.player.stop();}}
+                case (KeyEvent.VK_RIGHT) -> {if(this.player.getDirection() == 1) {this.player.stop();}}
+                case (KeyEvent.VK_SPACE) -> {
+                    player.stopShooting();
+                    emitter.emitterFrequency = 0;
+                }
+            }
         }
-
-        if (event.getKeyCode() == KeyEvent.VK_RIGHT && this.player.getDirection() == 1) {
-            this.player.stop();
+    }
+    @Override
+    public void keyTyped(KeyEvent event){
+        if (state == State.PLAYING) {
+            switch (event.getKeyCode()) {
+                case (KeyEvent.VK_SPACE) -> {
+                    if (!player.isShooting()) {
+                        player.startShooting();
+                    }
+                }
+            }
         }
     }
 }
