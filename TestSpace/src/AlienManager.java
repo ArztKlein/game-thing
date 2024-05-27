62ab92d5a7b4ee9cbca46cb527d0e7fb0da35f42
sami <samiabdullatif26@gmail.com>
2024-05-27 14:48:26 +1200

    Fix Alien and Player collision bug

--- TestSpace/src/AlienManager.java
+++ TestSpace/src/AlienManager.java
@@ -16,31 +16,30 @@ public class AlienManager {
 
     public void spawnAlien() {
         double spawnWidth = 400;
-        double offset = (TestSpace.WIDTH -spawnWidth)/2;
-        double spawnX =  offset + (Math.random() * spawnWidth); // Spawn randomly across the top width of the screen
+        double offset = (TestSpace.WIDTH - spawnWidth) / 2;
+        double spawnX = offset + (Math.random() * spawnWidth); // Spawn randomly across the top width of the screen
         Alien alien = new Alien(spawnX, 0, alienSprite);
         aliens.add(alien);
     }
+
     public void update(double dt) {
         Iterator<Alien> iterator = aliens.iterator();
         while (iterator.hasNext()) {
             Alien alien = iterator.next();
             alien.update(dt, player);
-
-            // Check collision with player
-            if (alien.checkCollision(player)) {
-                player.reduceHealth(10); // Reduce player's health
-                iterator.remove();
-            }
-
-            // Check if the alien has reached the player's height
-            if (alien.hasReachedPlayerHeight(player.getY())) {
+            if (alien.checkCollision(player) || alien.hasReachedPlayerHeight(player.getY())) {
+                if (alien.checkCollision(player)) {
+                    player.reduceHealth(5); // Reduce player's health on collision
+                }
                 iterator.remove();
             }
         }
     }
 
-    public List<Alien> getAliens(){return this.aliens;}
+    public List<Alien> getAliens() {
+        return this.aliens;
+    }
+
     public void draw(Graphics2D g) {
         for (Alien alien : aliens) {
             alien.draw(g);
