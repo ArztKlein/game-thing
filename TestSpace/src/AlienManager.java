c33e4764d970f721f84ffe8afa792d402ae65a09
sami <samiabdullatif26@gmail.com>
2024-05-29 06:01:30 +1200

    New Aliens

--- TestSpace/src/AlienManager.java
+++ TestSpace/src/AlienManager.java
@@ -7,22 +7,34 @@ public class AlienManager {
     private List<Alien> aliens;
     private Image alienSprite;
     private Player player;
+    private double elapsedTime;
 
     public AlienManager(Image alienSprite, Player player) {
         this.alienSprite = alienSprite;
         this.aliens = new ArrayList<>();
         this.player = player;
+        this.elapsedTime = 0;
     }
 
     public void spawnAlien() {
         double spawnWidth = 400;
         double offset = (TestSpace.WIDTH - spawnWidth) / 2;
         double spawnX = offset + (Math.random() * spawnWidth); // Spawn randomly across the top width of the screen
-        Alien alien = new Alien(spawnX, 0, alienSprite);
+        /*Alien alien = new Alien(spawnX, 0, alienSprite);
+        aliens.add(alien);*/
+        Alien alien;
+        if (elapsedTime >= 50){
+            alien = new LargeAlien(spawnX,0, alienSprite);
+        }else if (elapsedTime >= 30){
+            alien = new MediumAlien(spawnX,0, alienSprite);
+        }else {
+            alien = new Alien(spawnX, 0, alienSprite);
+        }
         aliens.add(alien);
     }
 
     public void update(double dt) {
+        elapsedTime += dt;
         Iterator<Alien> iterator = aliens.iterator();
         while (iterator.hasNext()) {
             Alien alien = iterator.next();
